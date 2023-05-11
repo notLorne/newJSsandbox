98c52c34519dc043c5ece1831d5aaf95f317e881
notLorne <113377659+notLorne@users.noreply.github.com>
2023-05-11 15:08:51 -0400

    later today

--- sandBoxing/js/WorldBuilder.js
+++ sandBoxing/js/WorldBuilder.js
@@ -1,5 +1,7 @@
-var nameBank1 = WorldNames.generateWordList(2, 4, 100);
+var npcBank1 = WorldNames.generateWordList(2, 4, 100);
+var npcBank2 = WorldNames.generateWordList(3, 4, 100);
+var npcBank3 = WorldNames.generateWordList(2, 3, 100);
 
-console.log(nameBank1);
+console.log(npcBank1);
 // console.log(nameBank2);
 // console.log(nameBank3);
